2715e5b0e6478928bca241dce6ff651db1e716f2
Akira Ouchi <akkiesoft@marokun.net>
2024-06-05 00:56:32 +0900

    customize campaigns for ejecra

--- plugin/campaign.rb
+++ plugin/campaign.rb
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-require_relative 'campaign/campaign_utils'
-
-Plugin.create :campaign do
-  on_give_continuous_login_bonus do |name, count|
-    Plugin::Campaign::Campaign.active_campaigns.each do |campaign|
-      campaign.daily(user_name: name, login_count: count)
-    end
-  end
-end
+#require_relative 'campaign/campaign_utils'
+#
+#Plugin.create :campaign do
+#  on_give_continuous_login_bonus do |name, count|
+#    Plugin::Campaign::Campaign.active_campaigns.each do |campaign|
+#      campaign.daily(user_name: name, login_count: count)
+#    end
+#  end
+#end
 
 
 
